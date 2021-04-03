49e433ca57a4cf6119571580633405f1cbb212f9
yuyuexiang <1622344876@qq.com>
2021-04-03 17:39:52 +0800

    2-6 启动文案修改提交

--- src/main/java/com/yyx/wiki/WikiApplication.java
+++ src/main/java/com/yyx/wiki/WikiApplication.java
@@ -1,13 +1,21 @@
 package com.yyx.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
+    private static  final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication sa=new SpringApplication(WikiApplication.class);
+        Environment em = sa.run(args).getEnvironment();
+        LOG.info("--------------启动okokokokokokokokok！！！--------------");
+        LOG.info("地址：\thttp://127.0.0.1:{}",em.getProperty("server.port"));
     }
 
 }
