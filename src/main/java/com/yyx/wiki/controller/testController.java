2eb2fcc2e6ddbdc4cb4283eb35897b37d32b07e8
yuyuexiang <1622344876@qq.com>
2021-04-03 21:32:35 +0800

    2-7 启动banner和第一个测试接口提交

---
+++ src/main/java/com/yyx/wiki/controller/testController.java
@@ -0,0 +1,22 @@
+package com.yyx.wiki.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @ClassName: testController
+ * @Description: 测试控制器层
+ * @author: yuyuexiang
+ * @date: 2021/4/3 17:47
+ */
+@RestController //前后端分离，接口只返回字符串给前端
+public class testController {
+
+//    @GetMapping("/demo") //只支持get请求，如果不是get请求访问此接口，报405错
+    @RequestMapping("/demo")
+    public String demo() {
+
+            return "Hellor World!";
+        }
+}
+
+
